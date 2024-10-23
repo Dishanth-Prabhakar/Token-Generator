f494ec63f02fc2e347a9521cde1d70fd41f22d4e
Dishanth P <dishanthprabhakar206@gmail.com>
2024-10-23 21:56:41 +0530

    function added to clear btn

--- tg-frontend/src/Pages/home.js
+++ tg-frontend/src/Pages/home.js
@@ -1,38 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function Home() {
+
+    // Initializing all the input field
+    const [blueToken, setBlueToken] = useState('');
+    const [bluePrefix, setBluePrefix] = useState('');
+    const [bluePerRow, setBluePerRow] = useState('');
+    const [redToken, setRedToken] = useState('');
+    const [redPrefix, setRedPrefix] = useState('');
+    const [redPerRow, setRedPerRow] = useState('');
+
+    // Function to clear the form
+    const handleClear = (e) => {
+        e.preventDefault();
+        setBlueToken('');
+        setBluePrefix('');
+        setBluePerRow('');
+        setRedToken('');
+        setRedPrefix('');
+        setRedPerRow('');
+    };
+
     return (
         <div>
             <h2>Token Generator Application</h2>
             <form>
                 <div>
                     <label>Number of blue tokens: </label>
-                    <input type='number'></input>
+                    <input type='number'
+                        value={blueToken}
+                        onChange={(e) => setBlueToken(e.target.value)} />
                 </div>
                 <div>
                     <label>Prefix for blue tokens: </label>
-                    <input type='text'></input>
+                    <input type='text' 
+                    value={bluePrefix} 
+                    onChange={(e) => setBluePrefix(e.target.value)} />
                 </div>
                 <div>
                     <label>Blue tokens per row: </label>
-                    <input type='number'></input>
+                    <input type='number' 
+                    value={bluePerRow} 
+                    onChange={(e) => setBluePerRow(e.target.value)} />
                 </div>
                 <div>
                     <label>Number of red tokens: </label>
-                    <input type='number'></input>
+                    <input type='number' 
+                    value={redToken} 
+                    onChange={(e) => setRedToken(e.target.value)} />
                 </div>
                 <div>
                     <label>Prefix for red tokens: </label>
-                    <input type='text'></input>
+                    <input type='text' 
+                    value={redPrefix}
+                    onChange={(e) => setRedPrefix(e.target.value)} />
                 </div>
                 <div>
                     <label>Red tokens per row: </label>
-                    <input type='number'></input>
+                    <input type='number' 
+                    value={redPerRow}
+                    onChange={(e) => setRedPerRow(e.target.value)} />
                 </div>
                 <div>
                     <button>Generate</button>
-                    <button>Clear</button>
+                    <button onClick={handleClear}>Clear</button>
                 </div>
             </form>
         </div>
