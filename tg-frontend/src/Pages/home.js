81e4efe36888c7a47e535435d4ab71fb3758dd3c
Dishanth P <dishanthprabhakar206@gmail.com>
2024-10-24 14:59:47 +0530

    UI styling done

--- tg-frontend/src/Pages/home.js
+++ tg-frontend/src/Pages/home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import './home.css'
 
 function Home() {
 
@@ -21,6 +21,7 @@ function Home() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
+        // checking whether the number of tokens and prefix field is empty or not
         if (blueToken === "" || bluePrefix === "" || redToken === "" || redPrefix === "") {
             setFormError("Number of tokens & prefix fields are mandatory to fill");
             return;
@@ -28,6 +29,7 @@ function Home() {
             setFormError('');
         }
 
+        // checking whether the tokens per row is not more 8 
         if (bluePerRow > 8) {
             setBlueError("The blue tokens per row should less than or equal to 8");
             return;
@@ -39,33 +41,40 @@ function Home() {
             setRedError("The red tokens per row should less than or equal to 8");
             return;
         } else {
-            setRedError('')
+            setRedError('');
         }
 
+        // assigning the default value for the tokens per row field (if the user is not provided)
         const bluePerRowValue = bluePerRow ? Number(bluePerRow) : 8;
         const redPerRowValue = redPerRow ? Number(redPerRow) : 8;
 
+        // to generate blue tokens
         const bluetok = Number(blueToken)
         const bluetokenlst = []
         for (let b = 1; b <= bluetok; b++) {
             bluetokenlst.push(`${bluePrefix}${b}`);
         }
 
+        // to generate red tokens
         const redtok = Number(redToken)
         const redtokenlst = []
         for (let r = 1; r <= redtok; r++) {
             redtokenlst.push(`${redPrefix}${r}`);
         }
 
+        // tokens generated and stored in a list
         setGeneratedtoken({ blue: bluetokenlst, red: redtokenlst });
 
+        // splitTokensIntoRows function call - for blue tokens
         const blueTokensSplit = splitTokensIntoRows(bluetokenlst, Number(bluePerRowValue));
         setSplitBlueTokens(blueTokensSplit);
 
+        // for red tokens
         const redTokensSplit = splitTokensIntoRows(redtokenlst, Number(redPerRowValue));
         setSplitRedTokens(redTokensSplit);
     };
 
+    // function to split the tokens into the row 
     const splitTokensIntoRows = (tokensArray, tokensPerRow) => {
         const rows = [];
         for (let i = 0; i < tokensArray.length; i += tokensPerRow) {
@@ -90,94 +99,111 @@ function Home() {
     };
 
     return (
-        <div>
-            <h2>Token Generator Application</h2>
+        <div className="container">
+
+            {/* Application heading */}
+            <h2 className="app-heading">Token Generator Application</h2>
             <div>
                 <span style={{ color: 'red' }}>
-                    <h5>{formError}</h5>
+                    <h5>{formError}</h5> {/* Error message display if number of tokens & prefix field is empty */}
                 </span>
             </div>
             <form onSubmit={handleSubmit}>
+
+                {/* Number of blue tokens - field */}
                 <div>
-                    <label>Number of blue tokens: </label>
+                    <label>Number of <span className="bluecolor">blue</span> tokens: </label>
                     <input type='number'
                         value={blueToken}
                         onChange={(e) => setBlueToken(e.target.value)} />
                 </div>
+
+                {/* Prefix for blue tokens - field */}
                 <div>
-                    <label>Prefix for blue tokens: </label>
+                    <label>Prefix for <span className="bluecolor">blue</span> tokens: </label>
                     <input type='text'
                         value={bluePrefix}
                         onChange={(e) => setBluePrefix(e.target.value)} />
                 </div>
+
+                {/* Blue tokens per row - field */}
                 <div>
-                    <label>Blue tokens per row: </label>
+                    <label><span className="bluecolor">Blue</span> tokens per row: </label>
                     <input type='number'
                         value={bluePerRow}
                         onChange={(e) => setBluePerRow(e.target.value)} />
                     <span style={{ color: 'red' }}>
-                        <h5>{blueError}</h5>
+                        <h5>{blueError}</h5> {/* Error message display if the tokens per row value is greater than 8 */}
                     </span>
                 </div>
-                
+
+                {/* Number of red tokens - field */}
                 <div>
-                    <label>Number of red tokens: </label>
+                    <label>Number of <span className="redcolor">red</span> tokens: </label>
                     <input type='number'
                         value={redToken}
                         onChange={(e) => setRedToken(e.target.value)} />
                 </div>
+
+                {/* Prefix for red tokens - field */}
                 <div>
-                    <label>Prefix for red tokens: </label>
+                    <label>Prefix for <span className="redcolor">red</span> tokens: </label>
                     <input type='text'
                         value={redPrefix}
                         onChange={(e) => setRedPrefix(e.target.value)} />
                 </div>
+
+                {/* Red tokens per row - field */}
                 <div>
-                    <label>Red tokens per row: </label>
+                    <label><span className="redcolor">Red</span> tokens per row: </label>
                     <input type='number'
                         value={redPerRow}
                         onChange={(e) => setRedPerRow(e.target.value)} />
                     <span style={{ color: 'red' }}>
-                        <h5>{redError}</h5>
+                        <h5>{redError}</h5> {/* Error message display if the tokens per row value is greater than 8 */}
                     </span>
                 </div>
-
                 <div>
-                    <button type="submit">Generate</button>
-                    <button onClick={handleClear}>Clear</button>
+                    <button type="submit" className="generatebtn">Generate</button>
+                    <button onClick={handleClear} className="clearbtn">Clear</button>
                 </div>
             </form>
 
-            <div>
-                {generatedTokens.blue.length > 0 ? (
-                    <div>
-                        {splitBlueTokens.map((row, rowIndex) => (
-                            <div key={rowIndex} style={{ marginBottom: '10px' }}>
-                                {row.map((token, tokenIndex) => (
-                                    <span key={tokenIndex} style={{ marginRight: '10px' }}>{token}</span>
-                                ))}
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <p>No blue tokens generated yet.</p>
-                )}
-            </div>
+            {/* Code to display the generated tokens */}
+            <div className="token-container">
+                <div>
+                    {generatedTokens.blue.length > 0 ? (
+                        <div>
+                            <h2 className="bluecolor">Blue tokens</h2>
+                            {splitBlueTokens.map((row, rowIndex) => (
+                                <div key={rowIndex} style={{ marginBottom: '10px' }}>
+                                    {row.map((token, tokenIndex) => (
+                                        <span key={tokenIndex} className="bluetoken-box">{token}</span>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <h3 style={{ marginBottom: '20px'}}><span className="bluecolor">Blue</span> tokens not yet generated</h3>
+                    )}
+                </div>
 
-            <div>
-                {generatedTokens.red.length > 0 ? (
-                    <div>
-                        {splitRedTokens.map((row, rowIndex) => (
-                            <div key={rowIndex} style={{ marginBottom: '10px' }}>
-                                {row.map((token, tokenIndex) => (
-                                    <span key={tokenIndex} style={{ marginRight: '10px' }}>{token}</span>
-                                ))}
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <p>No red tokens generated yet.</p>
-                )}
+                <div>
+                    {generatedTokens.red.length > 0 ? (
+                        <div>
+                            <h2 className="redcolor">Red tokens</h2>
+                            {splitRedTokens.map((row, rowIndex) => (
+                                <div key={rowIndex} style={{ marginBottom: '10px' }}>
+                                    {row.map((token, tokenIndex) => (
+                                        <span key={tokenIndex} className="redtoken-box">{token}</span>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <h3><span className="redcolor">Red</span> tokens not yet generated</h3>
+                    )}
+                </div>
             </div>
         </div>
     );
